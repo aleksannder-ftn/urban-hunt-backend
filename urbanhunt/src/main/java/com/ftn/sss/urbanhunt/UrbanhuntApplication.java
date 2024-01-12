677d7b767c879530aafd20241b497e671c32d509
Aleksander Zajac <aleksannder.z@gmail.com>
2024-01-12 21:47:13 +0100

    initial setup

---
+++ urbanhunt/src/main/java/com/ftn/sss/urbanhunt/UrbanhuntApplication.java
@@ -0,0 +1,13 @@
+package com.ftn.sss.urbanhunt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrbanhuntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrbanhuntApplication.class, args);
+	}
+
+}
---
+++ urbanhunt/src/test/java/com/ftn/sss/urbanhunt/UrbanhuntApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ftn.sss.urbanhunt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrbanhuntApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
