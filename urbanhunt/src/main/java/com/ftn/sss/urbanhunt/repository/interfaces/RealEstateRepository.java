8dbbf4a49a8d6ec79d222c075d3d57b6f165919c
stjepanoviccc <stjepanoviccc2021@gmail.com>
2024-01-23 23:11:11 +0100

    small fixes after zajec

--- urbanhunt/src/main/java/com/ftn/sss/urbanhunt/repository/interfaces/RealEstateRepository.java
+++ urbanhunt/src/main/java/com/ftn/sss/urbanhunt/repository/interfaces/RealEstateRepository.java
@@ -1,15 +1,9 @@
 package com.ftn.sss.urbanhunt.repository.interfaces;
 
-import com.ftn.sss.urbanhunt.model.Agent;
 import com.ftn.sss.urbanhunt.model.RealEstate;
-import com.ftn.sss.urbanhunt.model.enums.RealEstateType;
-import com.ftn.sss.urbanhunt.model.enums.TransactionType;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
